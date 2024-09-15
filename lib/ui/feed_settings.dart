940fe4a48f01b2e7cee8d126e28abeacb1566402
Kevin Fechtel <kevin.fechtel@circle-dev.de>
2024-09-15 13:36:35 +0200

    Remove search string

--- lib/ui/feed_settings.dart
+++ lib/ui/feed_settings.dart
@@ -15,12 +15,12 @@ class FeedSettings extends StatelessWidget {
   Widget build(BuildContext context) {
     FluxNewsState appState = context.watch<FluxNewsState>();
 
-    return FluxNewsSearchStatefulWrapper(onInit: () {
+    return FluxNewsFeedSettingsStatefulWrapper(onInit: () {
       initConfig(context, appState);
       appState.feedSettingsList = queryFeedsFromDB(appState, context);
     }, child: OrientationBuilder(builder: (context, orientation) {
       appState.orientation = orientation;
-      return searchLayout(context, appState);
+      return feedSettingsLayout(context, appState);
     }));
   }
 
@@ -34,7 +34,7 @@ class FeedSettings extends StatelessWidget {
     }
   }
 
-  Scaffold searchLayout(BuildContext context, FluxNewsState appState) {
+  Scaffold feedSettingsLayout(BuildContext context, FluxNewsState appState) {
     return Scaffold(
         appBar: AppBar(
           // set the title of the search page to search text field
@@ -57,16 +57,16 @@ class FluxNewsFeedSettingsBody extends StatelessWidget {
   }
 }
 
-class FluxNewsSearchStatefulWrapper extends StatefulWidget {
+class FluxNewsFeedSettingsStatefulWrapper extends StatefulWidget {
   final Function onInit;
   final Widget child;
-  const FluxNewsSearchStatefulWrapper({super.key, required this.onInit, required this.child});
+  const FluxNewsFeedSettingsStatefulWrapper({super.key, required this.onInit, required this.child});
   @override
   FluxNewsBodyState createState() => FluxNewsBodyState();
 }
 
 // extend class to save actual scroll state of the list view
-class FluxNewsBodyState extends State<FluxNewsSearchStatefulWrapper> {
+class FluxNewsBodyState extends State<FluxNewsFeedSettingsStatefulWrapper> {
   // init the state of FluxNewsBody to load the config and the data on startup
   @override
   void initState() {
